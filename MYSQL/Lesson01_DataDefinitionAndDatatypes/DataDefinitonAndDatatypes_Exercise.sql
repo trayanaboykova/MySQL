7aec3f606e35b6de15cdcf6a37056ccf1eb6147e
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-23 16:18:49 +0200

    Update DataDefinitonAndDatatypes_Exercise.sql

--- MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
+++ MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
@@ -23,15 +23,16 @@ FOREIGN KEY (town_id)
 REFERENCES towns(id);
 
 -- insert records in both tables
-INSERT INTO towns (name) VALUES
-('Sofia'),
-('Plovdiv'),
-('Varna');
+INSERT INTO towns (id, name) VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
 
-INSERT INTO minions (name, age, town_id) VALUES
-('Kevin', 22, 1),
-('Bob', 15, 3),
-('Steward', NULL, 2);
+INSERT INTO minions (id, name, age, town_id) VALUES
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2);
+
 
 
 
