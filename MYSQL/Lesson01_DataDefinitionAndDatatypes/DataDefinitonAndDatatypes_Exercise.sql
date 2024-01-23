e9efcc723cc6549adf96f6f9ab042a1152da0459
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-23 17:05:12 +0200

    Update DataDefinitonAndDatatypes_Exercise.sql

--- MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
+++ MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
@@ -62,12 +62,13 @@ INSERT INTO people (name, picture, height, weight, gender, birthdate, biography)
 
 -- create table users
 CREATE TABLE users (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT,
 username VARCHAR(30) unique,
 password VARCHAR(26),
 profile_picture BLOB,
 last_login_time TIME,
-is_deleted BOOLEAN
+is_deleted BOOLEAN,
+PRIMARY KEY (id)
 );
 
 INSERT INTO users (username, password, profile_picture, last_login_time, is_deleted) VALUES
@@ -77,5 +78,9 @@ INSERT INTO users (username, password, profile_picture, last_login_time, is_dele
 ('Augustine', 'password4', NULL, NULL, true),
 ('Betty', 'password5', NULL, NULL, false);
 
-
+-- change primary key
+ALTER TABLE users
+MODIFY id BIGINT NOT NULL,
+DROP PRIMARY KEY,
+ADD CONSTRAINT pk_users PRIMARY KEY (id, username);
 
