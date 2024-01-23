1e770b528ebd2ef9ec01ea615bdaa9035a0786c9
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-23 15:57:30 +0200

    Update DataDefinitonAndDatatypes_Exercise.sql

--- MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
+++ MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
@@ -12,3 +12,12 @@ CREATE TABLE towns (
  town_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
+
+-- alter minions table
+ALTER TABLE minions
+ADD COLUMN town_id INT;
+
+ALTER TABLE minions
+ADD CONSTRAINT fk_town
+FOREIGN KEY (town_id)
+REFERENCES towns(id);
