0f74dfdfaa62d539327c3a1c798e954e57d08f18
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-23 22:21:40 +0200

    Update DataDefinitonAndDatatypes_Exercise.sql

--- MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
+++ MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
@@ -274,33 +274,61 @@ name VARCHAR(50) NOT NULL,
 PRIMARY KEY (id)
 );
 
-
+INSERT INTO towns (name)
+VALUES ('Sofia'),
+       ('Plovdiv'),
+       ('Varna'),
+       ('Burgas');
  
  CREATE TABLE addresses (
 id INT NOT NULL AUTO_INCREMENT,
-address_text VARCHAR(50) NOT NULL,
-town_id INT NOT NULL,
+address_text VARCHAR(100),
+town_id INT,
 PRIMARY KEY (id)
 );
-
-
 
 CREATE TABLE departments (
 id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(50) NOT NULL,
+name VARCHAR(100),
 PRIMARY KEY (id)
 );
 
+INSERT INTO departments (name)
+VALUES
+('Engineering'),
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
 
 CREATE TABLE employees (
 id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(50) NOT NULL,
 middle_name VARCHAR(50),
 last_name VARCHAR(50) NOT NULL,
-job_title VARCHAR(50) NOT NULL, 
-department_id INT NOT NULL,
+job_title VARCHAR(50), 
+department_id INT,
 hire_date DATE,
 salary DOUBLE,
-address_id INT NOT NULL,
+address_id INT,
 PRIMARY KEY (id)
 );
+
+ALTER TABLE addresses
+ADD CONSTRAINT fk_AddressesTowns
+FOREIGN KEY(town_id) REFERENCES addresses (id);
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_EmployeesTowns
+FOREIGN KEY(department_id) REFERENCES departments (id);
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_Employees
+FOREIGN KEY(address_id) REFERENCES addresses (id);
+
+INSERT INTO employees (first_name, middle_name, last_name, job_title, department_id, hire_date, salary)
+VALUES ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00),
+       ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00),
+       ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25),
+       ('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00),
+       ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88);
