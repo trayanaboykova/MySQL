d82992e5dfccef0a74319e0e7f8c9dcb346ad433
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-23 15:53:33 +0200

    Update DataDefinitonAndDatatypes_Exercise.sql

--- MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
+++ MYSQL/Lesson01_DataDefinitionAndDatatypes/DataDefinitonAndDatatypes_Exercise.sql
@@ -1,2 +1,14 @@
 -- create database
 CREATE DATABASE minions;
+
+-- create tables
+CREATE TABLE minions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT
+);
+
+CREATE TABLE towns (
+ town_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
