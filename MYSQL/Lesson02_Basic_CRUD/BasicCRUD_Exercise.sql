51a0abb3ed57fda6782394456bcbc1974d546660
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-25 13:53:26 +0200

    Update BasicCRUD_Exercise.sql

--- MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
+++ MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
@@ -68,7 +68,8 @@ CREATE VIEW v_employees_job_titles AS
 SELECT CONCAT(first_name, ' ', IF(middle_name IS NOT NULL, CONCAT(middle_name, ' '), ''), last_name) AS full_name, job_title
 FROM employees;
 
-
+-- DISTINCT JOB TITLES
+SELECT DISTINCT job_title FROM employees ORDER BY job_title;
 
 
 
