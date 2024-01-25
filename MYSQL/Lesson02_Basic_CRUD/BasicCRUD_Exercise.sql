a35e786adf0b3c82e0f94d39e41b4384b26a0ced
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-25 14:03:12 +0200

    Update BasicCRUD_Exercise.sql

--- MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
+++ MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
@@ -81,6 +81,27 @@ SELECT first_name, last_name, hire_date FROM employees
 ORDER BY hire_date DESC
 LIMIT 7;
 
+-- INCREASE SALARIES
+SELECT department_id FROM departments
+WHERE name IN('Engineering', 'Tool Design', 'Marketing', 'Information Services');
+
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id IN(1, 2, 4, 11);
+
+SELECT salary FROM employees;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
