f3241bd380b08e02e182ba37f7943dd0997e188b
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-25 11:59:08 +0200

    Update BasicCRUD_Exercise.sql

--- MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
+++ MYSQL/Lesson02_Basic_CRUD/BasicCRUD_Exercise.sql
@@ -3,3 +3,8 @@ SELECT * FROM departments;
 
 -- FIND ALL DEPARTMENT NAMES 
 SELECT name FROM departments ORDER BY department_id;
+
+-- FIND SALARY FOR EACH EMPLOYEE
+ SELECT first_name, last_name, salary FROM employees ORDER BY employee_id;
+ 
+ 
