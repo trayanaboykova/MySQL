e094306765258c0c893406753902ed47730ff480
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-30 17:58:59 +0200

    Update BuiltIn_Functions_Exercise.sql

--- MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
+++ MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
@@ -1,3 +1,9 @@
 -- FIND NAMES OF ALL EMPLOYEES BY FIRST NAME
 SELECT first_name, last_name FROM employees
-WHERE first_name LIKE 'Sa%';
+WHERE first_name LIKE 'Sa%'
+ORDER BY employee_id;
+
+-- FIND NAMES OF ALL EMPLOYEES BY LAST NAME
+SELECT first_name, last_name FROM employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
