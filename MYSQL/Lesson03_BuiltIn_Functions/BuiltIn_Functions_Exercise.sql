b6b29ad0d23a44d157261e8d0e62b93d5c331697
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-30 18:57:55 +0200

    Update BuiltIn_Functions_Exercise.sql

--- MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
+++ MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
@@ -49,3 +49,11 @@ WHERE LENGTH(last_name) = 5;
 SELECT country_name, iso_code FROM countries
 WHERE country_name LIKE '%A%%A%%A%'
 ORDER BY iso_code;
+
+-- MIX OF PEAK AND RIVER NAMES
+SELECT peak_name, river_name,
+CONCAT(LOWER(peak_name), SUBSTRING(LOWER(river_name), 2)) AS 'mix' 
+FROM peaks, rivers
+WHERE RIGHT(peak_name, 1) = LEFT(river_name, 1)
+ORDER BY mix;
+
