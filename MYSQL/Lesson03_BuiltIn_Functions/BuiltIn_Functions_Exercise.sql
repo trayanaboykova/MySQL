9311d048135a64c5e15b18856f2bcb1956d68c37
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-30 18:05:17 +0200

    Update BuiltIn_Functions_Exercise.sql

--- MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
+++ MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
@@ -7,3 +7,9 @@ ORDER BY employee_id;
 SELECT first_name, last_name FROM employees
 WHERE last_name LIKE '%ei%'
 ORDER BY employee_id;
+
+-- FIND FIRST NAMES OF ALL EMPLOYEES
+SELECT first_name FROM employees
+WHERE (department_id = 3 OR department_id = 10) AND
+	  YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id;
