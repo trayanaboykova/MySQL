f38d368ecac1ce2ddf35ce398057d03f4c88250d
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-30 19:28:51 +0200

    Update BuiltIn_Functions_Exercise.sql

--- MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
+++ MYSQL/Lesson03_BuiltIn_Functions/BuiltIn_Functions_Exercise.sql
@@ -86,3 +86,12 @@ CASE
     ELSE 'EXTRA LONG'
 END AS 'Duration'
 FROM games;
+
+-- ORDERS TABLE
+SELECT
+    product_name,
+    order_date,
+    DATE_ADD(order_date, INTERVAL 3 DAY) AS pay_due_date,
+    DATE_ADD(order_date, INTERVAL 1 MONTH) AS deliver_due_date
+FROM
+    orders;
