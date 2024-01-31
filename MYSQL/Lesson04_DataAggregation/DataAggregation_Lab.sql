974ffc7f6181cc7571b8e2fd4c56ccb1862532b7
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-31 18:43:10 +0200

    Update DataAggregation_Lab.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Lab.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Lab.sql
@@ -5,7 +5,10 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- AVERAGE SALARY
-
+SELECT department_id, ROUND(AVG(salary), 2) AS 'Average salary'
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 -- MINIMUM SALARY
 
