c6548c7d084db65bee91902fb486e0666c203398
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-31 19:15:20 +0200

    Update DataAggregation_Lab.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Lab.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Lab.sql
@@ -23,3 +23,10 @@ FROM products
 WHERE category_id = 2 AND price > 8;
 
 -- MENU PRICES
+SELECT
+	category_id,
+    ROUND(AVG(price), 2) AS 'Average Price',
+    ROUND(MIN(price), 2) AS 'Cheapest Product',
+    MAX(price) AS 'Most Expensive Product'
+FROM products
+GROUP BY category_id;
