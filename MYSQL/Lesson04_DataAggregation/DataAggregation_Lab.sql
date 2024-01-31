3bcb2bced496eb904f3d39aebc298ea7af35ef35
Trayana Boykova <trayana.boykova@gmail.com>
2024-01-31 18:30:50 +0200

    Update DataAggregation_Lab.sql

---
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Lab.sql
@@ -0,0 +1,16 @@
+-- DEPARTMENTS INFO
+SELECT department_id, COUNT(id) AS 'Number of employees'
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
+
+-- AVERAGE SALARY
+
+
+-- MINIMUM SALARY
+
+
+-- APPETIZERS COUNT
+
+
+-- MENU PRICES
