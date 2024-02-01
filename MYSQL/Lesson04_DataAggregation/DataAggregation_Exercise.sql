a59fc129bb1dddb3b9ea6b3abb08bd1ae9982769
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 13:21:42 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -97,7 +97,10 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- EMPLOYEES MAXIMUM SALARIES
-
+SELECT department_id, MAX(salary) AS max_salary FROM employees
+GROUP BY department_id
+HAVING max_salary NOT BETWEEN 30000 AND 70000
+ORDER BY department_id;
 
 -- EMPLOYEES COUNT SALARIES
 
