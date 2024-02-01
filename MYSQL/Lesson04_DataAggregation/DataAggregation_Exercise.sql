7c8ebf1e59a83a6c710beb142917ec6071930547
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 13:40:57 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -108,7 +108,20 @@ FROM employees
 WHERE manager_id IS NULL;
 
 -- 3RD HIGHEST SALARY
-
+SELECT 
+    department_id,
+    (SELECT DISTINCT
+            salary
+        FROM
+            employees
+        WHERE
+            e.department_id = department_id
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 2) AS third_highest_salary
+FROM employees e
+GROUP BY department_id
+HAVING third_highest_salary IS NOT NULL
+ORDER BY department_id;
 
 -- SALARY CHALLANGE
 
