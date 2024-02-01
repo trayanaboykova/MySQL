3715ae39df89dd19ac84ca3035da5a1d2a048c99
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 12:14:39 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -6,7 +6,17 @@ SELECT MAX(magic_wand_size) AS longest_magic_wand
 FROM wizzard_deposits;
 
 -- LONGEST MAGIC PER DEPOSIT GROUPS
+SELECT deposit_group, MAX(magic_wand_size) AS longest_magic_wand
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY longest_magic_wand, deposit_group;
+
 -- SMALLEST DEPOSIT GROUP PER MAGIC WAND SIZE
+SELECT deposit_group
+FROM wizzard_deposits
+GROUP BY deposit_group
+LIMIT 1;
+
 -- DEPOSITS SUM
 -- DEPOSITS SUM FOR OLLIVANDER FAMILY
 -- DEPOSITS FILTER
