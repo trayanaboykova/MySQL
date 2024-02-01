86c59c93619b0048e4d03f0cca467ec6a88ab48e
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 13:16:51 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -81,7 +81,20 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- EMPLOYEES AVERAGE SALARIES
+CREATE TABLE highest_paid_employees
+SELECT * FROM employees
+WHERE salary > 30000;
 
+DELETE FROM highest_paid_employees
+WHERE manager_id = 42;
+
+UPDATE highest_paid_employees 
+SET salary = salary + 5000
+WHERE department_id = 1;
+
+SELECT department_id, AVG(salary) AS avg_salary FROM highest_paid_employees
+GROUP BY department_id
+ORDER BY department_id;
 
 -- EMPLOYEES MAXIMUM SALARIES
 
