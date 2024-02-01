36201dadc5c525bb442021609b6722f1cdd31a2a
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 14:04:12 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -124,7 +124,17 @@ HAVING third_highest_salary IS NOT NULL
 ORDER BY department_id;
 
 -- SALARY CHALLANGE
-
+SELECT first_name, last_name, department_id
+FROM employees e
+WHERE
+    salary > (SELECT 
+            AVG(salary)
+        FROM
+            employees
+        WHERE
+            department_id = e.department_id)
+ORDER BY department_id , employee_id
+LIMIT 10;
 
 -- DEPARTMENTS TOTAL SALARIES
 
