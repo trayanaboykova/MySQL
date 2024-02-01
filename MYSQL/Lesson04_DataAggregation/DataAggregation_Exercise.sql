a4ee21222d07b9a5f9090810536fb9f219634494
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-01 13:02:17 +0200

    Update DataAggregation_Exercise.sql

--- MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
+++ MYSQL/Lesson04_DataAggregation/DataAggregation_Exercise.sql
@@ -75,7 +75,10 @@ GROUP BY deposit_group, is_deposit_expired
 ORDER BY deposit_group DESC, is_deposit_expired;
 
 -- EMPLOYEES MINIMUM SALARIES
-
+SELECT department_id, MIN(salary) AS minimum_salary FROM employees
+WHERE department_id IN(2, 5, 7) AND hire_date > '2000-01-01'
+GROUP BY department_id
+ORDER BY department_id;
 
 -- EMPLOYEES AVERAGE SALARIES
 
