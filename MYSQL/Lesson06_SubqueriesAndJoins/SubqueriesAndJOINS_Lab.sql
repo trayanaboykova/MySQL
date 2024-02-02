b9ffd69a272656425730a7a563ad5b75584e273b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 23:21:10 +0200

    Update SubqueriesAndJOINS_Lab.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
@@ -1,5 +1,13 @@
 -- MANAGERS
-
+SELECT 
+	employee_id,
+    CONCAT(first_name, ' ', last_name) AS 'full_name',
+    departments.department_id, 
+    name AS 'department_name'
+FROM departments
+	JOIN employees ON departments.manager_id = employees.employee_id
+ORDER BY employee_id
+LIMIT 5;
 
 -- TOWNS AND ADDRESSES
 
