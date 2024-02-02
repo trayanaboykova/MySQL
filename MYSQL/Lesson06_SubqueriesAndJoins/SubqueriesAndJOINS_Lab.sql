2c072b9d27b4bb76cb892a2440ab1a9a60ef6d79
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 00:12:45 +0200

    Update SubqueriesAndJOINS_Lab.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
@@ -10,7 +10,7 @@ ORDER BY employee_id
 LIMIT 5;
 
 -- TOWNS AND ADDRESSES
--- SOLUTION 1
+-- SOLUTION 1 WITH JOIN
 SELECT 
 	a.town_id,
     t.name,
@@ -19,19 +19,26 @@ FROM addresses AS a
 	JOIN towns AS t ON a.town_id = t.town_id
 WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
 ORDER BY town_id, address_id;
--- SOLUTION 2
+-- SOLUTION 2 WITH INNER JOIN
 SELECT 
 	a.town_id,
     t.name,
     a.address_text
 FROM addresses AS a
-	JOIN towns AS t 
+	INNER JOIN towns AS t 
     ON a.town_id = t.town_id AND
     t.name IN ('San Francisco', 'Sofia', 'Carnation')
 ORDER BY town_id, address_id;
 
 -- EMPLOYEES WITHOUT MANAGERS
-
+SELECT
+	employee_id,
+    first_name,
+    last_name,
+    department_id,
+    salary
+FROM employees
+WHERE manager_id IS NULL;
 
 -- HIGH SALARY
 SELECT COUNT(*) AS 'count'
