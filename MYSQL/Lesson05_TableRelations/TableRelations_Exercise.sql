107d1207ed0cf6c2afc4faa01fb8f7a9fbe4466c
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 20:46:12 +0200

    Update TableRelations_Exercise.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Exercise.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Exercise.sql
@@ -202,3 +202,10 @@ CREATE TABLE agenda (
 );
 
 -- PEAKS IN RILLA
+USE geography;
+
+SELECT m.mountain_range, p.peak_name, p.elevation FROM peaks p
+JOIN mountains m 
+ON p.mountain_id = m.id
+WHERE m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
