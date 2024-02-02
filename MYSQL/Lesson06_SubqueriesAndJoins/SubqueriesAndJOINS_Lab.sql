b50aecffbb0cea3bd76c478b3daea0167f596fcc
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 23:48:50 +0200

    Update SubqueriesAndJOINS_Lab.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Lab.sql
@@ -10,7 +10,25 @@ ORDER BY employee_id
 LIMIT 5;
 
 -- TOWNS AND ADDRESSES
-
+-- SOLUTION 1
+SELECT 
+	a.town_id,
+    t.name,
+    a.address_text
+FROM addresses AS a
+	JOIN towns AS t ON a.town_id = t.town_id
+WHERE t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id, address_id;
+-- SOLUTION 2
+SELECT 
+	a.town_id,
+    t.name,
+    a.address_text
+FROM addresses AS a
+	JOIN towns AS t 
+    ON a.town_id = t.town_id AND
+    t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id, address_id;
 
 -- EMPLOYEES WITHOUT MANAGERS
 
