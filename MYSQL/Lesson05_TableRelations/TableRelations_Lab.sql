22d055c3eabe879f5a1b7b0dc87fb7c1489a2263
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 16:45:03 +0200

    Update TableRelations_Lab.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
@@ -42,3 +42,36 @@ CREATE TABLE peaks (
         REFERENCES mountains (id)
         ON DELETE CASCADE
 );
+
+-- PROJECT MANAGEMENT DB
+CREATE DATABASE project_management_db;
+
+CREATE TABLE clients
+(
+    id          INT(11) PRIMARY KEY AUTO_INCREMENT,
+    client_name VARCHAR(100)
+);
+CREATE TABLE projects
+(
+    id              INT(11) PRIMARY KEY AUTO_INCREMENT,
+    client_id       INT(11),
+    project_lead_id INT(11)
+);
+CREATE TABLE employees
+(
+    id         INT(11) PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name  VARCHAR(30),
+    project_id INT(11)
+);
+ALTER TABLE projects
+    ADD CONSTRAINT fk_projects_clients
+        FOREIGN KEY (client_id)
+            REFERENCES clients (id),
+    ADD CONSTRAINT fk_projects_employees
+        FOREIGN KEY (project_lead_id)
+            REFERENCES employees (id);
+ALTER TABLE employees
+    ADD CONSTRAINT fk_employees_projects
+        FOREIGN KEY (project_id)
+            REFERENCES projects (id);
