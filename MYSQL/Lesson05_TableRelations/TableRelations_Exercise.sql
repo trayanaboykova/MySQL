d7cd52aa54d13b38a1eb9aa389b3d8880440209c
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 18:20:11 +0200

    Update TableRelations_Exercise.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Exercise.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Exercise.sql
@@ -28,10 +28,69 @@ INSERT INTO people (first_name, salary, passport_id) VALUES
 ('Yana', 60200.00, 101);
 
 -- ONE-TO-MANY RELATIONSHIPS
+CREATE TABLE manufacturers(
+manufacturer_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) UNIQUE,
+established_on DATE
+);
 
+INSERT INTO manufacturers(name, established_on) VALUES 
+('BMW', '1916-03-01'),
+('Tesla', '2003-01-01'),
+('Lada', '1966-05-01');
+
+CREATE TABLE models(
+model_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50),
+manufacturer_id INT,
+FOREIGN KEY (manufacturer_id)
+REFERENCES manufacturers(manufacturer_id)
+);
+
+INSERT INTO models VALUES
+(101, 'X1', 1),
+(102, 'i6', 1),
+(103, 'Model S', 2),
+(104, 'Model X', 2),
+(105, 'Model 3', 2),
+(106, 'Nova', 3);
 
 -- MANY-TO-MANY RELATIONSHIPS
+CREATE TABLE students(
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50)
+);
 
+INSERT INTO students(student_id, name) VALUES
+(1, 'Mila'), 
+(2, 'Toni'), 
+(3, 'Ron');
+
+CREATE TABLE exams(
+exam_id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(50) 
+);
+
+INSERT INTO exams(exam_id, name) VALUES
+(101, 'Spring MVC'), 
+(102, 'Neo4j'), 
+(103, 'Oracle 11g');
+
+CREATE TABLE students_exams(
+student_id INT,
+exam_id INT,
+PRIMARY KEY(student_id, exam_id),
+FOREIGN KEY (student_id) REFERENCES students(student_id),
+FOREIGN KEY (exam_id) REFERENCES exams(exam_id)
+);
+
+INSERT INTO students_exams(student_id, exam_id) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103);
 
 -- SELF-REFERENCING
 
