13712022175e6fa7b62c1fd4e3549e4b934998ab
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 16:11:39 +0200

    Update TableRelations_Lab.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
@@ -30,4 +30,15 @@ FROM routes
 	JOIN campers ON routes.leader_id = campers.id;
 
 -- DELETE MOUNTAINS
-
+CREATE TABLE mountains (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+CREATE TABLE peaks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    mountain_id INT,
+    CONSTRAINT fk_peaks_mountains FOREIGN KEY (mountain_id)
+        REFERENCES mountains (id)
+        ON DELETE CASCADE
+);
