48c7b52aa2c7ac85d0c29997c6d8b02fbae5fe71
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 15:59:28 +0200

    Update TableRelations_Lab.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
@@ -21,7 +21,13 @@ FROM vehicles
 	JOIN campers ON vehicles.driver_id = campers.id;
 
 -- SOFTUNI HIKING
-
+SELECT
+	starting_point AS 'route_starting_point',
+    end_point AS 'route_end_point',
+    leader_id,
+	CONCAT(first_name, ' ', last_name) AS 'leader_name'
+FROM routes
+	JOIN campers ON routes.leader_id = campers.id;
 
 -- DELETE MOUNTAINS
 
