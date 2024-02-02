c9e1d050fa730dc18cf4a1785f46bab733ae5a9c
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 15:56:20 +0200

    Update TableRelations_Lab.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
@@ -7,12 +7,18 @@ CREATE TABLE peaks(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     mountain_id INT,
-    CONSTRAINT fk_peaks_mountain_id_mountains_id
+    CONSTRAINT fk_peaks_mountains
     FOREIGN KEY (mountain_id)
     REFERENCES mountains(id)
 );
--- TRIP ORGANIZATION
 
+-- TRIP ORGANIZATION
+SELECT 
+	vehicles.driver_id, 
+    vehicle_type ,
+    CONCAT(campers.first_name, ' ', campers.last_name) AS 'driver_name'
+FROM vehicles
+	JOIN campers ON vehicles.driver_id = campers.id;
 
 -- SOFTUNI HIKING
 
