88d4f91749c53b71d425d804eb653dab0722b5c4
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-02 14:46:57 +0200

    Update TableRelations_Lab.sql

--- MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
+++ MYSQL/Lesson05_TableRelations/TableRelations_Lab.sql
@@ -1,6 +1,16 @@
 -- MOUNTAINS AND PEAKS
-
-
+CREATE TABLE mountains (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+CREATE TABLE peaks(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    mountain_id INT,
+    CONSTRAINT fk_peaks_mountain_id_mountains_id
+    FOREIGN KEY (mountain_id)
+    REFERENCES mountains(id)
+);
 -- TRIP ORGANIZATION
 
 
