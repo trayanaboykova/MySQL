76d4eebae1f185ac8cee1df950d7312bb2dff619
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 22:07:03 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -7,6 +7,7 @@ BEGIN
     ORDER BY first_name, last_name, employee_id;
 END $
 
+DELIMITER ;
 CALL usp_get_employees_salary_above_35000();
 
 -- EMPLOYEES WITH SALARY ABOVE NUMBER
@@ -18,13 +19,37 @@ BEGIN
     ORDER BY first_name, last_name, employee_id;
 END $
 
+DELIMITER ;
+
 CALL usp_get_employees_salary_above(70000);
 
 -- TOWN NAMES STARTING WITH
+DELIMITER $
+CREATE PROCEDURE usp_get_towns_starting_with(symbol VARCHAR(20))
+BEGIN
+	SELECT name FROM towns
+    WHERE name LIKE CONCAT(symbol, '%')
+    ORDER BY name;
+END $
 
+DELIMITER ;
+
+CALL usp_get_towns_starting_with('s');
 
 -- EMPLOYEES FROM TOWN
+DELIMITER $
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(100))
+BEGIN
+	SELECT e.first_name, e.last_name FROM employees e 
+    JOIN addresses a ON e.address_id = a.address_id
+    JOIN towns t ON a.town_id = t.town_id
+    WHERE t.name = town_name
+    ORDER BY e.first_name, e.last_name, e.employee_id;
+END $
 
+DELIMITER ;
+
+CALL usp_get_employees_from_town('Sofia');
 
 -- SALARY LEVEL FUNCTION
 
