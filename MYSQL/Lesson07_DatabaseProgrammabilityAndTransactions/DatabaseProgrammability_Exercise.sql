7eba75aec4093c81b1b2604e76265c12eb62de9f
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 22:14:54 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -52,7 +52,25 @@ DELIMITER ;
 CALL usp_get_employees_from_town('Sofia');
 
 -- SALARY LEVEL FUNCTION
+DELIMITER $
+CREATE FUNCTION ufn_get_salary_level(salary DECIMAL(10,4))
+RETURNS VARCHAR(20)
+READS SQL DATA
+BEGIN
+	DECLARE result VARCHAR(20);
+    IF (salary < 30000) THEN 
+		SET result := 'Low';
+	ELSEIF (salary >= 30000 AND salary <= 50000) THEN
+		SET result := 'Average';
+	ELSE
+		SET result := 'High';
+	END IF;
+    RETURN result;
+END $
 
+DELIMITER ;
+
+SELECT ufn_get_salary_level(55000);
 
 -- EMPLOYEES BY SALARY LEVEL
 
