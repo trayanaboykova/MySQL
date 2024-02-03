5ddf126645cde4cd6c64187bf30f891753d6b920
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 20:25:14 +0200

    Update DatabaseProgrammability_Lab.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
@@ -14,7 +14,23 @@ BEGIN
 END $
 
 -- EMPLOYEES PROMOTION
+DELIMITER $
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR (50))
+BEGIN
+	UPDATE employees e
+    JOIN departments d
+    ON e.department_id = d.department_id
+    SET e.salary = e.salary * 1.05
+    WHERE d.name = department_name;
+END $
 
+SELECT employee_id, first_name, salary FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id
+WHERE d.name = 'Finance'
+ORDER BY first_name, salary;
+
+CALL usp_raise_salaries('Finance');
 
 -- EMPLOYEES PROMOTION BY ID
 
