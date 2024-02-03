0b0ca02bcd5d764fce26b14f292507a668686167
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 12:44:27 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -51,6 +51,16 @@ WHERE e.hire_date > '1999-01-01' AND d.name IN ('Sales', 'Finance')
 ORDER BY e.hire_date ASC;
 
 -- EMPLOYEE WITH PROJECTS
+SELECT e.employee_id, e.first_name, p.name AS project_name
+FROM employees e
+JOIN employees_projects ep
+ON e.employee_id = ep.employee_id
+JOIN projects p
+ON ep.project_id = p.project_id
+WHERE p.start_date > '2002-08-13' AND p.end_date IS NULL
+ORDER BY e.first_name, p.name
+LIMIT 5;
+
 -- EMPLOYEE 24
 -- EMPLOYEE MANAGER
 -- EMPLOYEE SUMMARY
