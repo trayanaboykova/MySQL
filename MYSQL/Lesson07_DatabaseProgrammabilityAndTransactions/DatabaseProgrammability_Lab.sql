2c7576d9314298533740b170b082d6080fae3dc5
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 21:01:36 +0200

    Update DatabaseProgrammability_Lab.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
@@ -33,6 +33,21 @@ ORDER BY first_name, salary;
 CALL usp_raise_salaries('Finance');
 
 -- EMPLOYEES PROMOTION BY ID
+DELIMITER $
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+	START TRANSACTION;
+    IF((SELECT COUNT(*) FROM employees WHERE employee_id = id) <> 1)
+    THEN ROLLBACK;
+    ELSE
+		UPDATE employees
+        SET salary = salary * 1.05
+        WHERE employee_id = id;
+        COMMIT;
+    END IF;
+END $
 
+CALL usp_raise_salary_by_id(17);
+SELECT * FROM employees WHERE employee_id = 17;
 
 -- TRIGGERED
