3825485cc01b45b3a3619756570810d4e8b1f7cb
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 23:55:35 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -201,7 +201,26 @@ DELIMITER ;
 CALL usp_withdraw_money(1, 10);
 
 -- MONEY TRANSFER
+DELIMITER $
+CREATE PROCEDURE usp_transfer_money(acc_id INT, target_id INT, money_amount DECIMAL(19,4))
+BEGIN
+	START TRANSACTION;
+	IF ((SELECT COUNT(*) FROM accounts WHERE id = acc_id) <> 1 
+		OR (SELECT COUNT(*) FROM accounts WHERE id = target_id) <> 1 
+        OR acc_id = target_id
+        OR (SELECT balance FROM accounts WHERE id = acc_id) <= money_amount 
+        OR money_amount < 0) 
+        THEN ROLLBACK;
+	ELSE
+		UPDATE accounts SET balance = balance - money_amount WHERE id = acc_id;
+		UPDATE accounts SET balance = balance + money_amount WHERE id = target_id;
+        COMMIT;
+	END IF;
+END $
 
+DELIMITER ;
+
+CALL usp_transfer_money(1, 10);
 
 -- LOG ACCOUNTS TRIGGER
 
