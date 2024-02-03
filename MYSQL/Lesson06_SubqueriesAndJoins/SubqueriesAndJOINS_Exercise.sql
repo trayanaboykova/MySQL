9875f84a62d2cdd4433e57a043d995ec6bb3a3ad
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 12:14:02 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -1,5 +1,10 @@
 -- EMPLOYEE ADDRESS
-
+SELECT e.employee_id, e.job_title, a.address_id, a.address_text 
+FROM employees AS e
+JOIN addresses AS a 
+ON e.address_id = a.address_id
+ORDER BY a.address_id
+LIMIT 5;
 
 -- ADDRESSES WITH TOWNS
 -- SALES EMPLOYEE
