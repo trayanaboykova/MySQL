52c98d43a02d7304f7f55981b87ed7d97ca61381
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-04 00:08:48 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -223,6 +223,27 @@ DELIMITER ;
 CALL usp_transfer_money(1, 10);
 
 -- LOG ACCOUNTS TRIGGER
+CREATE TABLE logs(
+	log_id INT PRIMARY KEY AUTO_INCREMENT,
+	account_id INT NOT NULL,
+	old_sum DECIMAL(19,4),
+	new_sum DECIMAL(19,4)
+);
 
+DELIMITER $
+
+CREATE TRIGGER tr_update
+AFTER UPDATE
+ON accounts
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs(account_id, old_sum, new_sum) 
+    VALUES (old.id, old.balance, new.balance);
+END $
+
+DELIMITER ; 
+
+CALL usp_deposit_money(1,10);
+SELECT * FROM logs;
 
 -- EMAILS TRIGGER
