97204c2217add3ad8daf6d870f5749c68aa80fe2
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 22:30:40 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -73,7 +73,17 @@ DELIMITER ;
 SELECT ufn_get_salary_level(55000);
 
 -- EMPLOYEES BY SALARY LEVEL
+DELIMITER $
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(20))
+BEGIN
+	SELECT first_name, last_name FROM employees
+    WHERE salary_level = ufn_get_salary_level(salary)
+    ORDER BY first_name DESC, last_name DESC;
+END $
 
+DELIMITER ;
+
+CALL usp_get_employees_by_salary_level('High');
 
 -- DEFINE FUNCTION
 
