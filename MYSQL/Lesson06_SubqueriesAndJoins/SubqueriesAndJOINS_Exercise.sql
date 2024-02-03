9ba4538781a74b95ae1497331e5117486c8df8fd
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 14:09:54 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -187,5 +187,24 @@ ORDER BY c.country_name
 LIMIT 5;
 
 -- CONTINENTS AND CURRENCIES
+SELECT 
+    c.continent_code,
+    c.currency_code,
+    COUNT(*) AS currency_usage
+FROM
+    countries c
+GROUP BY c.continent_code , c.currency_code
+HAVING currency_usage > 1
+    AND currency_usage = (SELECT 
+        COUNT(*) AS max_usage
+    FROM
+        countries
+    WHERE
+        continent_code = c.continent_code
+    GROUP BY currency_code
+    ORDER BY max_usage DESC
+    LIMIT 1)
+ORDER BY c.continent_code , c.currency_code;
+
 -- COUNTRIES WITHOUT ANY MOUNTAINS 
 -- HIGHEST PEAK AND LONGEST RIVER BY COUNTRY
