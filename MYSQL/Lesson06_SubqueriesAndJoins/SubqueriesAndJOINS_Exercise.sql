6525b18d273ac9a70f5b496c43d6ee81286e5c2b
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 12:59:08 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -62,6 +62,16 @@ ORDER BY e.first_name, p.name
 LIMIT 5;
 
 -- EMPLOYEE 24
+SELECT e.employee_id, e.first_name,
+IF(YEAR(p.start_date) >= 2005, NULL, p.name) AS project_name
+FROM employees e
+JOIN employees_projects ep
+ON e.employee_id = ep.employee_id
+JOIN projects p
+ON ep.project_id = p.project_id
+WHERE e.employee_id = 24
+ORDER BY project_name;
+
 -- EMPLOYEE MANAGER
 -- EMPLOYEE SUMMARY
 -- MIN AVERAGE SALARY
