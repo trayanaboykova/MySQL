1e9b0479ed00160f5eb51d830ce1b9aea186a005
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 23:06:03 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -113,7 +113,23 @@ DELIMITER ;
 CALL usp_get_holders_full_name();
 
 -- PEOPLE WITH BALANCE HIGHER THAN
+DELIMITER $
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(target_salary DECIMAL(19,4))
+BEGIN
+	SELECT ah.first_name, ah.last_name
+    FROM account_holders ah
+    JOIN accounts a ON ah.id = a.account_holder_id
+    WHERE target_salary < (SELECT SUM(balance) 
+						   FROM accounts
+                           WHERE account_holder_id = ah.id
+                           GROUP BY account_holder_id)
+    GROUP BY ah.id
+    ORDER BY ah.id;
+END $
 
+DELIMITER ;
+
+CALL usp_get_holders_with_balance_higher_than(7000);
 
 -- FUTURE VALUE FUNCTION
 
