af740ab9c7eaaae8db1d5131482bc9d8caa4bd00
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 19:47:24 +0200

    Update DatabaseProgrammability_Lab.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
@@ -1,5 +1,17 @@
 -- COUNT EMPLOYEES BY TOWN
-
+DELIMITER  $
+CREATE FUNCTION ufn_count_employees_by_town(town_name VARCHAR(50)) RETURNS INT
+DETERMINISTIC
+BEGIN
+	DECLARE result INT;
+    SET @result := (SELECT COUNT(*) FROM employees e
+					JOIN addresses a 
+                    ON e.address_id = a.address_id
+                    JOIN towns t
+                    ON a.town_id = t.town_id
+                    WHERE t.name = town_name);
+    RETURN @result;
+END $
 
 -- EMPLOYEES PROMOTION
 
