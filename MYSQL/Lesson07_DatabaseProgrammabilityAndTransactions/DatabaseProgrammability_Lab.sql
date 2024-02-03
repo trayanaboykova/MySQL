51f658b9a919030dbf2475d7f33aeda00867ed6e
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 21:03:26 +0200

    Update DatabaseProgrammability_Lab.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Lab.sql
@@ -51,3 +51,23 @@ CALL usp_raise_salary_by_id(17);
 SELECT * FROM employees WHERE employee_id = 17;
 
 -- TRIGGERED
+CREATE TABLE deleted_employees(
+employee_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+middle_name VARCHAR(20),
+job_title VARCHAR(50),
+department_id INT,
+salary DOUBLE
+);
+
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+INSERT INTO deleted_employees (first_name,last_name,
+middle_name,job_title,department_id,salary)
+VALUES(OLD.first_name,OLD.last_name,OLD.middle_name,
+OLD.job_title,OLD.department_id,OLD.salary);
+END;
