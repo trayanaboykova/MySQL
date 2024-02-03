961381cf80c3e07af7dc1f8bcf1e96fa9eb35b14
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 14:24:50 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -217,3 +217,22 @@ WHERE
     mc.mountain_id IS NULL;
 
 -- HIGHEST PEAK AND LONGEST RIVER BY COUNTRY
+SELECT 
+    c.country_name AS country_count,
+    MAX(p.elevation) AS highest_peak_elevation,
+    MAX(r.length) AS longest_river_length
+FROM
+    countries c
+        LEFT JOIN
+    mountains_countries mc ON c.country_code = mc.country_code
+        LEFT JOIN
+    mountains m ON mc.mountain_id = m.id
+        LEFT JOIN
+    peaks p ON m.id = p.mountain_id
+        LEFT JOIN
+    countries_rivers cr ON c.country_code = cr.country_code
+        LEFT JOIN
+    rivers r ON cr.river_id = r.id
+GROUP BY c.country_name
+ORDER BY highest_peak_elevation DESC , longest_river_length DESC , c.country_name
+LIMIT 5;
