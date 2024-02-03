26b949ed50b369753cbeb14f25ffcaddffc2192e
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 22:58:13 +0200

    Update DatabaseProgrammability_Exercise.sql

--- MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
+++ MYSQL/Lesson07_DatabaseProgrammabilityAndTransactions/DatabaseProgrammability_Exercise.sql
@@ -100,7 +100,17 @@ SELECT 'Sofia' REGEXP '^[oistmiahf]+$';
 SELECT ufn_is_word_comprised('bobr', 'Rob');
 
 -- FIND FULL NAME
+DELIMITER $
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+	SELECT CONCAT(first_name, ' ', last_name) AS full_name
+    FROM account_holders
+    ORDER BY full_name;
+END $
 
+DELIMITER ;
+
+CALL usp_get_holders_full_name();
 
 -- PEOPLE WITH BALANCE HIGHER THAN
 
