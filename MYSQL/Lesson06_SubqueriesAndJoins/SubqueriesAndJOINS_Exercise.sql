c14bb18b5e71ee3ea3b2c2077129399d43e5b477
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-03 12:38:17 +0200

    Update SubqueriesAndJOINS_Exercise.sql

--- MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
+++ MYSQL/Lesson06_SubqueriesAndJoins/SubqueriesAndJOINS_Exercise.sql
@@ -43,6 +43,13 @@ ORDER BY e.employee_id DESC
 LIMIT 3;
 
 -- EMPLOYEE HIRED AFTER
+SELECT e.first_name, e.last_name, e.hire_date, d.name AS dept_name
+FROM employees AS e
+JOIN departments d
+ON e.department_id = d.department_id
+WHERE e.hire_date > '1999-01-01' AND d.name IN ('Sales', 'Finance')
+ORDER BY e.hire_date ASC;
+
 -- EMPLOYEE WITH PROJECTS
 -- EMPLOYEE 24
 -- EMPLOYEE MANAGER
