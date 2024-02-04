6e4eaf38d28d0aeb2212d3c39df8009dd201978d
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-04 16:55:07 +0200

    Update Exam12.sql

--- MYSQL/ExamPreparation/E12_Database Basics MYSQL Retake Exam - 11.12.2023/Exam12.sql
+++ MYSQL/ExamPreparation/E12_Database Basics MYSQL Retake Exam - 11.12.2023/Exam12.sql
@@ -50,3 +50,13 @@ CREATE TABLE flights_passengers (
     FOREIGN KEY (passenger_id)
         REFERENCES passengers (id)
 );
+
+-- INSERT
+INSERT INTO airplanes (model, passengers_capacity, tank_capacity, cost)
+SELECT
+    CONCAT(REVERSE(first_name), '797') AS model,
+    LENGTH(last_name) * 17 AS passengers_capacity,
+    id * 790 AS tank_capacity,
+    LENGTH(first_name) * 50.6 AS cost
+FROM passengers
+WHERE id <= 5;
