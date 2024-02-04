66e1871cda82ab97b0ca10553b4f57d67f17a279
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-04 17:34:04 +0200

    Update Exam12.sql

--- MYSQL/ExamPreparation/E12_Database Basics MYSQL Retake Exam - 11.12.2023/Exam12.sql
+++ MYSQL/ExamPreparation/E12_Database Basics MYSQL Retake Exam - 11.12.2023/Exam12.sql
@@ -81,4 +81,102 @@ FROM
     airplanes
 ORDER BY
     cost DESC, id DESC;
+    
+-- FLIGHTS FROM 2022
+SELECT
+    flight_code,
+    departure_country,
+    airplane_id,
+    departure
+FROM
+    flights
+WHERE
+    YEAR(departure) = 2022
+ORDER BY
+    airplane_id ASC, flight_code ASC
+LIMIT 20;
 
+-- PRIVATE FLIGHTS
+SELECT
+    CONCAT(UPPER(SUBSTRING(last_name, 1, 2)), country_id) AS flight_code,
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    country_id
+FROM
+    passengers
+WHERE
+    id NOT IN (SELECT DISTINCT passenger_id FROM flights_passengers)
+ORDER BY
+    country_id ASC;
+
+-- LEADING DESTINATIONS
+SELECT
+    c.name AS name,
+    c.currency AS currency,
+    COUNT(fp.passenger_id) AS booked_tickets
+FROM
+    countries c
+JOIN
+    flights f ON c.id = f.destination_country
+LEFT JOIN
+    flights_passengers fp ON f.id = fp.flight_id
+GROUP BY
+    c.id
+HAVING
+    booked_tickets >= 20
+ORDER BY
+    booked_tickets DESC;
+
+-- PARTS OF THE DAY 
+SELECT
+    flight_code,
+    departure,
+    CASE
+        WHEN TIME(departure) BETWEEN '05:00:00' AND '11:59:59' THEN 'Morning'
+        WHEN TIME(departure) BETWEEN '12:00:00' AND '16:59:59' THEN 'Afternoon'
+        WHEN TIME(departure) BETWEEN '17:00:00' AND '20:59:59' THEN 'Evening'
+        ELSE 'Night'
+    END AS day_part
+FROM
+    flights
+ORDER BY
+    flight_code DESC;
+    
+-- NUMBER OF FLIGHTS
+DELIMITER $
+
+CREATE FUNCTION udf_count_flights_from_country(countryName VARCHAR(50))
+RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE flightsCount INT;
+
+    SELECT COUNT(*)
+    INTO flightsCount
+    FROM flights
+    WHERE departure_country = (SELECT id FROM countries WHERE name = countryName);
+
+    RETURN flightsCount;
+END $
+
+DELIMITER ;
+
+
+-- DELAY FLIGHT
+DELIMITER $
+
+CREATE PROCEDURE udp_delay_flight(IN flightCode VARCHAR(50))
+BEGIN
+    DECLARE departureTime DATETIME;
+    SELECT departure INTO departureTime
+    FROM flights
+    WHERE flight_code = flightCode;
+    
+    SET departureTime = DATE_ADD(departureTime, INTERVAL 30 MINUTE);
+    
+    UPDATE flights
+    SET departure = departureTime, has_delay = 1
+    WHERE flight_code = flightCode;
+    
+END $
+
+DELIMITER ;
