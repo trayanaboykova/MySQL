26b51ffd7f1ca03c01bbf80c95e3be7c43d279e4
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-04 13:11:33 +0200

    Update Exam13.sql

--- MYSQL/ExamPreparation/E13_Database Basics MYSQL Regular Exam - 10.06.2023/Exam13.sql
+++ MYSQL/ExamPreparation/E13_Database Basics MYSQL Regular Exam - 10.06.2023/Exam13.sql
@@ -171,13 +171,26 @@ END $
 
 DELIMITER ;
 
+SELECT 
+    c.name,
+    UDF_AVERAGE_ALUMNI_GRADE_BY_COURSE_NAME('Quantum Physics') AS average_alumni_grade
+FROM
+    courses c
+WHERE
+    c.name = 'Quantum Physics';
+
 -- GRADUATE STUDENTS
-	
+DELIMITER $
 
+CREATE PROCEDURE udp_graduate_all_students_by_year(year_started INT)
+BEGIN
+	UPDATE students s
+    JOIN students_courses sc ON s.id = sc.student_id
+    JOIN courses c ON sc.course_id = c.id
+    SET is_graduated = 1
+    WHERE YEAR(c.start_date) = year_started;
+END $
 
+DELIMITER ;
 
-
-
-
-
-
+CALL udp_graduate_all_students_by_year(2017); 
