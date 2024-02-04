90ecc784923b3ae746ff7f14eab8b622296d8fdc
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-04 12:19:05 +0200

    Update Exam13.sql

--- MYSQL/ExamPreparation/E13_Database Basics MYSQL Regular Exam - 10.06.2023/Exam13.sql
+++ MYSQL/ExamPreparation/E13_Database Basics MYSQL Regular Exam - 10.06.2023/Exam13.sql
@@ -56,6 +56,18 @@ CREATE TABLE students_courses(
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
-
-
-
+-- INSERT
+INSERT INTO courses(name, 
+					duration_hours, 
+                    start_date, 
+                    teacher_name, 
+                    description, 
+                    university_id)
+SELECT CONCAT(teacher_name, ' course'),
+	   LENGTH(name) / 10,
+	   DATE_ADD(start_date, INTERVAL 5 DAY),
+       REVERSE(teacher_name),
+       CONCAT('Course ', teacher_name, REVERSE(description)),
+       DAY(start_date)
+FROM courses
+WHERE id <= 5;
