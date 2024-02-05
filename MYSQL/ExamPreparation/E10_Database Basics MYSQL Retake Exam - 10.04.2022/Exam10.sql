a0b4bf6fb93779c50470940bf7d107b1631c5af2
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-05 19:48:38 +0200

    Update Exam10.sql

--- MYSQL/ExamPreparation/E10_Database Basics MYSQL Retake Exam - 10.04.2022/Exam10.sql
+++ MYSQL/ExamPreparation/E10_Database Basics MYSQL Retake Exam - 10.04.2022/Exam10.sql
@@ -53,7 +53,7 @@ CREATE TABLE movies_actors(
     actor_id INT, 
     CONSTRAINT fk_movies_actors_movies
 	FOREIGN KEY (movie_id) REFERENCES movies(id),
-    CONSTRAINT fk_movies_actors__actors
+    CONSTRAINT fk_movies_actors_actors
 	FOREIGN KEY (actor_id) REFERENCES actors(id)
 );
 
@@ -62,9 +62,63 @@ CREATE TABLE genres_movies(
     movie_id INT, 
 	CONSTRAINT fk_genres_movies_genres 
 	FOREIGN KEY (genre_id) REFERENCES genres(id),
-    CONSTRAINT fk_genres_movies__movies
+    CONSTRAINT fk_genres_movies_movies
     FOREIGN KEY (movie_id) REFERENCES movies(id)
 );
+
+-- INSERT
+INSERT INTO actors (first_name, last_name, birthdate, height, awards, country_id)
+SELECT
+    REVERSE(first_name),
+    REVERSE(last_name),
+    DATE_SUB(birthdate, INTERVAL 2 DAY),
+    height + 10,
+    country_id,
+    (SELECT id FROM countries WHERE name = 'Armenia')
+FROM actors
+WHERE id <= 10;
+
+-- UPDATE
+UPDATE movies_additional_info
+SET runtime = runtime - 10
+WHERE id >= 15 AND id <= 25;
+
+-- DELETE
+DELETE FROM countries
+WHERE id NOT IN (SELECT DISTINCT country_id FROM movies);
+
+-- COUNTRIES
+SELECT id, name, continent, currency
+FROM countries
+ORDER BY currency DESC, id;
+
+-- OLD MOVIES
+SELECT m.id, m.title, mai.runtime, mai.budget, mai.release_date
+FROM movies_additional_info mai
+JOIN movies m ON mai.id = m.movie_info_id
+WHERE YEAR(mai.release_date) BETWEEN 1996 AND 1999
+ORDER BY mai.runtime ASC, m.id ASC
+LIMIT 20;
+
+-- MOVIE CASTING
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    CONCAT(
+        REVERSE(last_name),
+        LENGTH(last_name),
+        '@cast.com'
+    ) AS email,
+    2022 - YEAR(birthdate) AS age,
+    height
+FROM actors
+WHERE id NOT IN (
+    SELECT DISTINCT actor_id
+    FROM movies_actors
+)
+ORDER BY height ASC;
+
+
+
 
 
 
