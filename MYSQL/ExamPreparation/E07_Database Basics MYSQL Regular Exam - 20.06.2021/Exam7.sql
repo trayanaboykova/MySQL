8f4722d863e74a49690c27bbb98295c25bae6172
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-06 19:28:39 +0200

    Update Exam7.sql

--- MYSQL/ExamPreparation/E07_Database Basics MYSQL Regular Exam - 20.06.2021/Exam7.sql
+++ MYSQL/ExamPreparation/E07_Database Basics MYSQL Regular Exam - 20.06.2021/Exam7.sql
@@ -2,6 +2,60 @@ CREATE DATABASE exam_07;
 USE exam_07;
 
 -- TABLE DESIGN
+CREATE TABLE addresses(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE categories(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(10) NOT NULL 
+);
+
+CREATE TABLE clients(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE drivers(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age INT NOT NULL,
+	rating FLOAT DEFAULT 5.5
+);
+
+CREATE TABLE cars(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    make VARCHAR(20) NOT NULL, 
+    model VARCHAR(20),
+    year INT NOT NULL DEFAULT 0,
+    mileage INT DEFAULT 0,
+    `condition` CHAR(1) NOT NULL,
+    category_id INT NOT NULL, 
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE TABLE courses(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    from_address_id INT NOT NULL,
+    start DATETIME NOT NULL, 
+    car_id INT, 
+    client_id INT,
+    bill DECIMAL(10,2) DEFAULT 10,
+	FOREIGN KEY (from_address_id) REFERENCES addresses(id),
+	FOREIGN KEY (car_id) REFERENCES cars(id),
+	FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+CREATE TABLE cars_drivers(
+	car_id INT NOT NULL,
+    driver_id INT NOT NULL,
+    PRIMARY KEY (car_id, driver_id),
+    FOREIGN KEY (car_id) REFERENCES cars(id),
+    FOREIGN KEY (driver_id) REFERENCES drivers(id)
+);
 
 -- INSERT
 -- UPDATE
