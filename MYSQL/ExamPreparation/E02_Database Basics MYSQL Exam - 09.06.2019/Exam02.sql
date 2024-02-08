84cbae55b4f2aa65214a23c4aa4514d2e2012ee9
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-08 18:22:52 +0200

    Update Exam02.sql

--- MYSQL/ExamPreparation/E02_Database Basics MYSQL Exam - 09.06.2019/Exam02.sql
+++ MYSQL/ExamPreparation/E02_Database Basics MYSQL Exam - 09.06.2019/Exam02.sql
@@ -71,9 +71,47 @@ WHERE id NOT IN (
 );
 
 -- CLIENTS
+SELECT id, full_name
+FROM clients
+ORDER BY id ASC;
+
 -- NEWBIES
+SELECT id,
+       CONCAT(first_name, ' ', last_name) AS full_name,
+       CONCAT('$', salary) AS salary,
+       started_on
+FROM employees
+WHERE salary >= 100000 AND started_on >= '2018-01-01'
+ORDER BY salary DESC, id;
+
 -- CARDS AGAINST HUMANITY
+SELECT c.id,
+       CONCAT(c.card_number, ' : ', cl.full_name) AS card_token
+FROM cards c
+JOIN bank_accounts ba ON c.bank_account_id = ba.id
+JOIN clients cl ON ba.client_id = cl.id
+ORDER BY c.id DESC;
+
 -- TOP 5 EMPLOYEES
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS name,
+       e.started_on,
+       COUNT(ec.client_id) AS count_of_clients
+FROM employees e
+LEFT JOIN employees_clients ec ON e.id = ec.employee_id
+GROUP BY e.id
+ORDER BY count_of_clients DESC, e.id ASC
+LIMIT 5;
+
 -- BRANCH CARDS
+SELECT b.name AS name, COUNT(c.id) AS count_of_cards
+FROM branches b
+LEFT JOIN employees e ON b.id = e.branch_id
+LEFT JOIN employees_clients ec ON e.id = ec.employee_id
+LEFT JOIN clients cl ON ec.client_id = cl.id
+LEFT JOIN bank_accounts ba ON cl.id = ba.client_id
+LEFT JOIN cards c ON ba.id = c.bank_account_id
+GROUP BY b.id
+ORDER BY count_of_cards DESC, name;
+
 -- EXTRACT CARD'S COUNT
 -- CLIENT INFO
