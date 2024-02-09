4276c9d5498688d42c25c1f21cbdca364b40e14f
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-09 21:44:13 +0200

    Update Exam09.sql

--- MYSQL/ExamPreparation/E09_Database Basics MYSQL Regular Exam - 13.02.2022/Exam09.sql
+++ MYSQL/ExamPreparation/E09_Database Basics MYSQL Regular Exam - 13.02.2022/Exam09.sql
@@ -77,9 +77,72 @@ DELETE FROM customers
 WHERE id NOT IN (SELECT DISTINCT customer_id FROM orders);
 
 -- CATEGORIES
+SELECT id, name
+FROM categories
+ORDER BY name DESC;
+
 -- QUANTITY
+SELECT id, brand_id, name, quantity_in_stock
+FROM products
+WHERE price > 1000 AND quantity_in_stock < 30
+ORDER BY quantity_in_stock ASC, id ASC;
+
 -- REVIEW
+SELECT id, content, rating, picture_url, published_at
+FROM reviews
+WHERE content LIKE 'My%' AND LENGTH(content) > 61
+ORDER BY rating DESC;
+
 -- FIRST CUSTOMERS
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, address, order_datetime
+FROM customers
+JOIN orders ON customers.id = orders.customer_id
+WHERE YEAR(order_datetime) <= 2018
+ORDER BY full_name DESC;
+
 -- BEST CATEGORIES
+SELECT 
+    COUNT(products.id) AS items_count,
+    categories.name,
+    SUM(products.quantity_in_stock) AS total_quantity
+FROM 
+    categories
+JOIN 
+    products ON categories.id = products.category_id
+GROUP BY 
+    categories.id, categories.name
+ORDER BY 
+    items_count DESC, total_quantity ASC
+LIMIT 5;
+
 -- EXTRACT CLIENT CARDS COUNT
+DELIMITER //
+CREATE FUNCTION udf_customer_products_count(fname VARCHAR(30)) RETURNS INT
+BEGIN
+    DECLARE total_count INT;
+    
+    SELECT COUNT(orders_products.product_id)
+    INTO total_count
+    FROM customers
+    JOIN orders ON customers.id = orders.customer_id
+    JOIN orders_products ON orders.id = orders_products.order_id
+    WHERE customers.first_name = fname;
+    
+    RETURN total_count;
+END //
+DELIMITER ;
+
 -- REDUCE PRICE
+DELIMITER $$
+
+CREATE PROCEDURE udp_reduce_price(category_name VARCHAR(50))
+BEGIN
+    UPDATE products AS p
+    JOIN reviews AS r ON r.id = p.review_id
+    JOIN categories AS c ON c.id = p.category_id
+    SET p.price = ROUND(p.price * 0.7, 2)
+    WHERE r.rating < 4
+    AND c.name = category_name;
+END $$
+
+DELIMITER ;
