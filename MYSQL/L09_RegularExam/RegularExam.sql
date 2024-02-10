32f294232ea09ab4bf05386aef28c79b96f05e3a
Trayana Boykova <trayana.boykova@gmail.com>
2024-02-10 10:00:02 +0200

    Update RegularExam.sql

--- MYSQL/L09_RegularExam/RegularExam.sql
+++ MYSQL/L09_RegularExam/RegularExam.sql
@@ -53,3 +53,24 @@ CREATE TABLE countries_preserves(
     FOREIGN KEY (country_id) REFERENCES countries(id),
     FOREIGN KEY (preserve_id) REFERENCES preserves(id)
 );
+
+-- INSERT
+INSERT INTO preserves (name, latitude, longitude, area, type, established_on)
+SELECT CONCAT(name, ' is in South Hemisphere') AS name,
+       latitude,
+       longitude,
+       area * id AS area,
+       LOWER(type) AS type,
+       established_on
+FROM preserves
+WHERE latitude < 0;
+
+-- UPDATE
+UPDATE workers
+SET salary = salary + 500
+WHERE position_id IN (5, 8, 11, 13);
+
+-- DELETE 
+DELETE FROM preserves
+WHERE established_on IS NULL;
+
